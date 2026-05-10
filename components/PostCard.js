e27220098ae18aa8f7bd3ae27e39f01d15d65654
ileilo1215 <ileilo1215@github.com>
2026-05-10 20:49:23 +0900

    style: add emergency icon to rescue category label

--- components/PostCard.js
+++ components/PostCard.js
@@ -76,8 +76,8 @@ export default function PostCard({ post, objectPosition = 'center 20%' }) {
     'Play': '놀이',
     'Class': '교실',
     'News': '뉴스',
-    'Street Life': '구조',
-    'Rights': '동물권'
+    'Street Life': '🚨 구조',
+    'Rights': '🚨 동물권'
   };
 
   const translatedCategory = categoryMap[post.category] || post.category;
